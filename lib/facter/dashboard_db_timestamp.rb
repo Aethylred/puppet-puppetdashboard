4029231fae3b8ffe56d57c643d1bd0b7d7bc896b
Aaron Hicks <hicksa@landcareresearch.co.nz>
2014-05-02 15:43:44 +1200

    dashboard_db_timestamp needs to return nil if the database is missing

--- lib/facter/dashboard_db_timestamp.rb
+++ lib/facter/dashboard_db_timestamp.rb
@@ -3,7 +3,12 @@ Facter.add(:dashboard_db_timestamp) do
   install_dir = Facter.value('dashboard_install_dir')
   setcode do
     if install_dir
-      Facter::Util::Resolution.exec("rake -f #{install_dir}/Rakefile RAILS_ENV=production db:version 2> /dev/null").match(/^Current version: (\d*)$/)[1]
+      db_timestamp = Facter::Util::Resolution.exec("rake -f #{install_dir}/Rakefile RAILS_ENV=production db:version 2> /dev/null")
+      if db_timestamp
+        db_timestamp.match(/^Current version: (\d*)$/)[1]
+      else
+        nil
+      end
     else
       nil
     end
